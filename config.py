bdad3790ee81265b4480d3e6185be57bdd2884c9
Benjamin Boche <benjamin.boche@gmx.de>
2025-01-28 20:44:53 +0100

    simplify config class and add parameters

--- config.py
+++ config.py
@@ -16,6 +16,7 @@ class Config():
     model_num_classes:int
     model_pretrained:bool
     model_freeze:bool
+    model_save_path:Union[str, os.PathLike]
 
     # Optimizer
     optim_name:OptimizerOptions
@@ -25,40 +26,20 @@ class Config():
 
     # Loss
     loss_name:LossOptions
-    loss_class_weights:List[float]
+    loss_compute_class_weights:bool
 
     # Loop
     loop_epochs:int
 
-    #Data
+    # Data
     data_root:Union[str, os.PathLike]
 
-    def __init__(self, model_name:TimmModelOptions, model_num_classes:int, model_pretrained:bool, model_freeze:bool,
-                 optim_name:OptimizerOptions, optim_learning_rate:float, optim_weight_decay:float, optim_momentum:Optional[float], 
-                 loss_name:LossOptions, loss_class_weights:List[float],
-                 loop_epochs:int,
-                 data_root:Union[str, os.PathLike]):
-        if not(model_num_classes == len(loss_class_weights)):
-            raise ValueError("Number of classes in parameter 'model_num_classes' should be the same as the length of parameter 'loss_class_weight'. \
-                              I.e. 'model_num_classes == len(loss_class_weights)' should be true.")
-        # Model
-        self.model_name = model_name
-        self.model_num_classes = model_num_classes
-        self.model_pretrained = model_pretrained
-        self.model_freeze = model_freeze
+    # Augmentation
+    augmentation_prob:float
 
-        # Optimizer
-        self.optim_name = optim_name
-        self.optim_learning_rate = optim_learning_rate
-        self.optim_weight_decay = optim_weight_decay
-        self.optim_momentum = optim_momentum
+    #DEFAULT PARAMETERS
+    # Loss
+    loss_class_weights:Optional[List[float]] = None
 
-        # Loss
-        self.loss_name = loss_name
-        self.loss_class_weights = loss_class_weights
-
-        # Loop
-        self.loop_epochs = loop_epochs
-
-        #Data
-        self.data_root = data_root
+    # Augmentation
+    augmentation_viz:bool = False
